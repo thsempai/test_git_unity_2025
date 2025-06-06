1599ceaf9383bde8721d00eb7208731a3d2219be
Thomas Stassin <thomas.stassin@interface3.be>
2025-06-06 12:08:53 +0200

    change in exo14.py

--- exo14.py
+++ exo14.py
@@ -1,10 +1,10 @@
-word = input("Mot: ")
+word = input("Word!!!!!!!!!!!!!!!!!!!!!!!!!!!: ")
 count_words = 1
 
 while word != "end":
     if word[0] == "t":
         print(f"{word}!")
     word = input("Mot: ")
-    count_words += 1
+    count_words += 2
 
 print(f"Nombre de mots: {count_words}")
